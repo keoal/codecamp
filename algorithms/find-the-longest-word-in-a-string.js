1896d96e74ba846b5e9ae1563a8799b534b5db24
keoal <alankeogh@gmail.com>
2020-09-24 15:34:23 +0100

    Update find-the-longest-word-in-a-string.js

--- algorithms/find-the-longest-word-in-a-string.js
+++ algorithms/find-the-longest-word-in-a-string.js
@@ -1,11 +1,13 @@
 function findLongestWordLength(str) {
+  //split str
   let words = str.split(" ");
   console.log(words);
-
+//get the length of each word
   let lengths = words.map(word => word.length)
   console.log(lengths);
-  //spread the array into seperate arguments
+  //spread the array into seperate arguments and find the biggest word
   return Math.max(...lengths)
 }
 
 findLongestWordLength("The quick brown fox jumped over the lazy dog");
+
