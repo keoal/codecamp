74b1d2c1aa161d82d90b717052ccf8fd6eb7fbd9
keoal <alankeogh@gmail.com>
2020-11-16 17:59:08 +0000

    Update smallest-common-multiple.js

--- intermediate-algorithm-scripting/smallest-common-multiple.js
+++ intermediate-algorithm-scripting/smallest-common-multiple.js
@@ -1,19 +1,22 @@
 function smallestCommons(arr) {
   //create a new array with all the numbers in the range.
   let fullArr=[]
-  for (var i = arr[0]; i <= arr[1]; i++) {
+  //loop through the array if i is equal to the biggest number and is greater than or equal to the smallest number then iterate from largest to smallest number and push into the array. 
+  for (var i = Math.max(arr[0], arr[1]); i >= Math.min(arr[0], arr[1]); i--) {
     fullArr.push(i);
 }
+  console.log(1);
   console.log(arr);
   console.log(fullArr);
 
-  //Find the greatest common divisor first (GCD)
+//Find the greatest common divisor first (GCD)
+//https://learnersbucket.com/examples/algorithms/program-to-find-the-gcd-of-two-numbers-in-javascript/
   function gcd(x, y) {
     // Implements the Euclidean Algorithm
     if (y === 0) return x;
     else return gcd(y, x % y);
   }
-
+  
   //then calculate the lcm (least common multiple)
   let lcm = fullArr[0];
   for (i = 1; i < fullArr.length; i++) {
@@ -23,7 +26,8 @@ function smallestCommons(arr) {
   console.log(lcm);
   return lcm;
 
+
 }
 
 
-smallestCommons([1,6]);
+smallestCommons([5,1]);
