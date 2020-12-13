dce9f2e53e5110cc748802601f62690155729a58
keoal <alankeogh@gmail.com>
2020-12-13 18:54:40 +0000

    Update make-a-person.js

--- intermediate-algorithm-scripting/make-a-person.js
+++ intermediate-algorithm-scripting/make-a-person.js
@@ -1,3 +1,16 @@
+*/
+Intermediate Algorithm Scripting: Make a Person
+Fill in the object constructor with the following methods below:
+
+getFirstName()
+getLastName()
+getFullName()
+setFirstName(first)
+setLastName(last)
+setFullName(firstAndLast)
+Run the tests to see the expected output for each method. The methods that take an argument must accept only one argument and it has to be a string. These methods must be the only available means of interacting with the object.
+/*
+
 var Person = function(firstAndLast) {
   // Only change code below this line
   // Complete the method below and implement the others similarly
