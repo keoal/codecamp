c6b0d764489d5abb5988132aeb02212af5bd95d5
keoal <alankeogh@gmail.com>
2020-12-14 22:33:06 +0000

    Create palindrome-checker.js

---
+++ javascript-algorith-and-data-structure-projects/palindrome-checker.js
@@ -0,0 +1,15 @@
+function palindrome(str) {
+  var word = [...str];
+  //str.replace(/[^a-zA-Z ]/g,);
+  console.log(word.pop());
+  console.log(word.length);
+
+  var rWord = [];
+console.log(rWord.push(word.pop()))
+  //if word == rWord
+  //return true;
+}
+
+
+
+palindrome("racecar");
