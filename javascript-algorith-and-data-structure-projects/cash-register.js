04fcda35ac09b955a36f3e61a31091a7f359c264
keoal <alankeogh@gmail.com>
2020-12-22 15:32:13 +0000

    Create cash-register.js

---
+++ javascript-algorith-and-data-structure-projects/cash-register.js
@@ -0,0 +1,13 @@
+function checkCashRegister(price, cash, cid) {
+  //create object for return with status & change keys
+
+  let output = {
+    status: "",
+    change: []
+  }
+  
+  //var change;
+  return output;
+}
+
+checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
