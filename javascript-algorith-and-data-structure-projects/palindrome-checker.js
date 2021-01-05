15043fd491f489b89f1f6a025627dec92f4e97b0
keoal <alankeogh@gmail.com>
2021-01-05 13:20:18 +0000

    Update palindrome-checker.js

--- javascript-algorith-and-data-structure-projects/palindrome-checker.js
+++ javascript-algorith-and-data-structure-projects/palindrome-checker.js
@@ -1,10 +1,20 @@
 function palindrome(str) {
+  
+  //First clean the string an make it lowercase removing non-alpha numeric characters.
   var cleansed = str.toLowerCase().replace(/[\W_ ]/g, "");
+  console.log(cleansed);
+
+  //Convert the cleansed string into an array
   var word = [...cleansed];
+  //console.log("word", word);
+
+  //Create an empty array to store the reversed word
   var rWord = [];
   //str.replace(/[^a-zA-Z ]/g,);
   //console.log(word.pop());
   //console.log(word.length);
+ 
+ //Loop through the word backwards and push each letter into the empty array 
  for (var i = word.length -1; i >= 0; i--){
    console.log("for", word[i]);
   // word[i].pop;
@@ -12,12 +22,16 @@ function palindrome(str) {
  }
 console.log(rWord);
 
+//rejoin the reverse word into a string
 var rWordStr = rWord.join("");
+
 //var cleansed = rWordStr.replace(/[^a-zA-Z ]/g, "");
 console.log("rWord String: ", rWordStr);
 console.log("str: ", cleansed);
   //if word == rWord
   //return true;
+
+  //Use an if else statement to check if the word is a palindrome by comparing the cleansed string to the reversed word.
   if(rWordStr === cleansed){
     console.log("Yay! its a palindrome!!!")
     return true;
@@ -28,5 +42,5 @@ console.log("str: ", cleansed);
 }
 
 
-
-palindrome("ra ce%car");
+//Input for function argument
+palindrome("ta ce%car");
