67951492631d4c93cfdd85570c40b7449e5b3c33
keoal <alankeogh@gmail.com>
2021-01-05 13:53:58 +0000

    Update roman-numeral-converter.js

--- javascript-algorith-and-data-structure-projects/roman-numeral-converter.js
+++ javascript-algorith-and-data-structure-projects/roman-numeral-converter.js
@@ -1,4 +1,6 @@
 function convertToRoman(num) {
+ 
+ //First create two variables to list each roman numeral and the equivalent number so we can compare
  let roman = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 
  let decimal = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
