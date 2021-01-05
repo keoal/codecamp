3660955617e776c5f0084b5d93af7d9e40018caf
keoal <alankeogh@gmail.com>
2021-01-05 14:19:45 +0000

    Update caesars-cypher.js

--- javascript-algorith-and-data-structure-projects/caesars-cypher.js
+++ javascript-algorith-and-data-structure-projects/caesars-cypher.js
@@ -2,28 +2,39 @@
 
 function rot13(str) {
 
+//Create a new variable which converts str to an upper case array
 var array = [...str.toUpperCase()];
 //console.log(array);
-var latin = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"];
 
-var cypher = ["N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
-//loop through cypher until letter is found until last letter in str is reached
-for(var i = 0; i < array.length; i++){
+var alpha1 = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M"];
+
+var alpha2 = ["N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+//Loop through array using a for loop
+for(var item = 0; item < array.length; item++){
   //console.log(array[i]);
-  for (var j = 0; j < latin.length; j++) {
-    //console.log(latin[j]);
-    if (array[i] === latin[j]){ //if array letter is the same as latin letter array letter is equal to cypher letter.
-        array[i] = cypher[j];
-    } else if (array[i] === cypher[j]){// otherwise if array letter is the same as cypher letter array letter is then equal to latin letter
-        array[i] = latin[j];
+  //Loop through the first alphabet array 
+  for (var index = 0; index < alpha1.length; index++) {
+    //console.log(alpha1[index]);
+    //If the item from the array loop matches the index of the firt alphabet array 
+    if (array[item] === alpha1[index]){ 
+        //then the item from the array is equal to the index from the second alphabet array
+        array[item] = alpha2[index];
+        //otherwise if the item from the array is equal to the index of the second alphabet array 
+    } else if (array[item] === alpha2[index]){
+        //Then the array item is equal to the index from the first alphabet array
+        array[item] = alpha1[index];
     }
   }
 
   
 }
- //console.log(str);
- console.log(array.join(""));//join all array characters back into a string
-  return str;
+ console.log(str);
+ console.log(array.join(""));
+  return array.join("");
 }
 rot13("SERR PBQR PNZC");
+rot13("SERR CVMMN!");
+rot13("SERR YBIR?");
+rot13("GUR DHVPX OEBJA SBK WHZCF BIRE GUR YNML QBT."); 
